73ab21be3bc9a5dd78e24c5b2176f26f842a31f7
shruti9923 <shrutijain9923@gmail.com>
2020-01-25 16:01:57 +0530

    front end

---
+++ static/script/form.js
@@ -0,0 +1,66 @@
+window.onscroll = function() {myFunction()};
+
+function myFunction() {
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+    document.getElementById("nav12").classList.remove("navbar-transparent");
+    document.getElementById("nav12").classList.add("navbar-solid");
+  } else {
+    document.getElementById("nav12").classList.add("navbar-transparent");
+    document.getElementById("nav12").classList.remove("navbar-solid");
+    }
+  }
+
+function showMessage(){
+      document.getElementById("message").style.display = "block";
+}
+
+function hideMessage(){
+    document.getElementById("message").style.display = "none";
+}
+
+function validateForm() {
+  // Validate lowercase letters
+
+  var letter = document.getElementById("letter");
+  var myInput = document.getElementById("regPassword");
+  var capital = document.getElementById("capital");
+  var number = document.getElementById("number");
+  var length = document.getElementById("length");
+
+  var lowerCaseLetters = /[a-z]/g;
+  if(myInput.value.match(lowerCaseLetters)) {
+    letter.classList.remove("invalid");
+    letter.classList.add("valid");
+  } else {
+    letter.classList.remove("valid");
+    letter.classList.add("invalid");
+}
+
+var upperCaseLetters = /[A-Z]/g;
+  if(myInput.value.match(upperCaseLetters)) {
+    capital.classList.remove("invalid");
+    capital.classList.add("valid");
+  } else {
+    capital.classList.remove("valid");
+    capital.classList.add("invalid");
+  }
+
+  // Validate numbers
+  var numbers = /[0-9]/g;
+  if(myInput.value.match(numbers)) {
+    number.classList.remove("invalid");
+    number.classList.add("valid");
+  } else {
+    number.classList.remove("valid");
+    number.classList.add("invalid");
+  }
+
+  // Validate length
+  if(myInput.value.length >= 8) {
+    length.classList.remove("invalid");
+    length.classList.add("valid");
+  } else {
+    length.classList.remove("valid");
+    length.classList.add("invalid");
+  }
+}
